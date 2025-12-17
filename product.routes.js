f5e114d3479429b5ca1039fe57bc1defc73b9dd1
Volodymyr Lichakov <v.o.lichakov@student.csn.khai.edu>
2025-12-17 19:51:42 +0200

    add express router, middleware, error handling + EXTRA TASK

---
+++ product.routes.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const products = require('./products');
+const { blockSpecialBrand } = require('./middleware');
+
+const router = express.Router();
+
+// handle get request for path /products
+router.get('/products', (request, response) => {
+   return response.json(products);
+});
+
+// handle get request for path /products/:brand
+router.get('/products/:brand', blockSpecialBrand, (request, response) => {
+   const { brand } = request.params; // Access the brand parameter from the URL
+
+   // Filter products based on the brand parameter
+   const filteredProducts = products.filter(product => product.brand === brand);
+
+   response.json(filteredProducts); // Send the filtered products as a JSON response
+});
+
+router.get('/productswitherror', (request, response) => {
+   let err = new Error("processing error ")
+   err.statusCode = 400
+   throw err
+});
+
+
+module.exports = router;
+
+// handle get request for path /products/id/:id
+router.get('/products/id/:id', (request, response, next) => {
+   const { id } = request.params;
+
+   const product = products.find(p => p.id === Number(id));
+
+   if (!product) {
+      const err = new Error('Product not found');
+      err.statusCode = 404;
+      return next(err);
+   }
+
+   response.json(product);
+});
